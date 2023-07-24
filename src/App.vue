79e8426e74cbc4902ce0cf6b51c675f96b63df9d
LuisB1717 <luisbarboz17@gmail.com>
2023-07-24 13:26:36 -0500

    Create sidebar component and add styles

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,33 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <SideBar></SideBar>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SideBar from "./components/SideBar";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    SideBar,
+  },
+};
 </script>
 
 <style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  height: 100vh;
+  background-color: #e3f4ff;
+  
+  
 }
 </style>
---
+++ src/components/SideBar.vue
@@ -0,0 +1,50 @@
+<template>
+  <nav class="navbar">
+    <span class="name">GEOAPP</span>
+
+    <ul class="list">
+      <li>Inicio</li>
+      <li>Países</li>
+      <li>Continentes</li>
+    </ul>
+  </nav>
+</template>
+
+<style>
+.navbar {
+  position: fixed;
+  width: 300px;
+  height: 100%;
+  background-color: #676767;
+  color: white;
+  padding: 20px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.name {
+  font-size: 30px;
+  font-weight: bold;
+  padding: 1rem;
+}
+
+.list {
+  list-style: none;
+  font-size: 25px;
+  font-weight: bold;
+  margin-top: 2rem;
+  cursor: pointer;
+}
+
+.list li {
+  margin: 1rem 0;
+  padding: 0.5rem 2.5rem;
+}
+
+.list li:hover {
+  background-color: white;
+  color: #676767;
+  border-radius: 7px;
+}
+</style>
