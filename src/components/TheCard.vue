2ba704131facc79e7eabef77c21f341fa2aee158
LuisB1717 <luisbarboz17@gmail.com>
2023-07-24 22:00:36 -0500

    Create component card

---
+++ src/components/TheCard.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="item-card">
+    <img
+      class="img-item"
+      src="https://elviajerofeliz.com/wp-content/uploads/2019/02/Ciudades-de-Espan%CC%83a.jpg"
+      alt=""
+    />
+
+    <div class="item-info">
+      <img
+        src="https://img.asmedia.epimg.net/resizer/LQyBk5T2TfVttC_yVM8n5HuEYpM=/1472x828/cloudfront-eu-central-1.images.arcpublishing.com/diarioas/53YSJXSIZFHNTBV52Z4AMKISUM.png"
+        alt=""
+      />
+      <div>
+        <p class="country-name">Spain</p>
+        <p class="continent-name">Europe</p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style>
+.item-card {
+  width: 300px;
+  background-color: white;
+  border-radius: 50px;
+  box-shadow: 1px 1px 10px #757575;
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+}
+.img-item {
+  width: 100%;
+  border-radius: 50px 50px 0 0;
+}
+
+.item-info {
+  display: flex;
+  padding: 15px 35px;
+  align-items: center;
+}
+
+.item-info img {
+  width: 60px;
+  height: 40px;
+  margin-right: 20px;
+}
+
+.country-name {
+  font-size: 25px;
+  font-weight: 800;
+  color: #0098ff;
+}
+
+.continent-name {
+  font-size: 20px;
+  font-weight: 500;
+}
+
+.item-card:hover {
+  .item-info {
+    background-color: #0098ff;
+    border-radius: 0 0 50px 50px;
+  }
+  .country-name,
+  .continent-name {
+    color: white;
+  }
+}
+</style>
---
+++ src/components/TheSearch.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="search-container">
+    <input
+      type="text"
+      class="search-input"
+      placeholder="Escribe el país que deseas ver"
+    />
+    <button class="search-button">
+      <img src="../assets/lupa.png" alt="" /> Buscar
+    </button>
+  </div>
+</template>
+
+<style>
+.search-container {
+  width: 100%;
+  max-width: 700px;
+  background-color: white;
+  display: flex;
+  align-items: center;
+  border-radius: 60px;
+  padding: 7px 25px;
+  box-shadow:  1px 2px 4px #757575; ;
+ 
+}
+
+.search-input {
+  background: transparent;
+  flex: 1;
+  border: 0;
+  outline: none;
+  padding: 24px;
+  color: #757575;
+}
+
+.search-button {
+  border: 0;
+  border-radius: 20px;
+  padding: 7px 25px;
+  background-color: #0098ff;
+  cursor: pointer;
+  color: white;
+  font-size: 20px;
+  display: flex;
+  align-items: center;
+  gap: 10px;
+}
+
+.search-button img {
+  width: 30px;
+}
+</style>
