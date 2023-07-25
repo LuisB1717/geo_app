38b7308235bb46ea468100f48f01884318e4a3da
LuisB1717 <luisbarboz17@gmail.com>
2023-07-25 10:47:44 -0500

    Add regenerator-runtine

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
+import "core-js/stable"
+import "regenerator-runtime/runtime"
 import Vue from 'vue'
 import App from './App.vue'
+import { createProvider } from './vue-apollo'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+  apolloProvider: createProvider(),
+  render: h => h(App)
 }).$mount('#app')
